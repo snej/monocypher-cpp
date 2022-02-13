383b1ed913f6069d5f58f6f82498dfaa055275ee
Jens Alfke <jens@mooseyard.com>
2022-02-13 15:33:02 -0800

    Added a `monocypher.h` shim / Improved CMake file
    It's no longer necessary for clients to add .../vendor/monocypher/src
    to their include-path.
    CMake can now build the library itself, not just tests.

---
+++ include/monocypher.h
@@ -0,0 +1,12 @@
+//
+//  monocypher.h
+//
+//  Monocypher-Cpp: Unofficial idiomatic C++17 wrapper for Monocypher
+//  <https://monocypher.org>
+//
+//  This is just a shim so that you can include the regular Monocypher C header
+//  without having to also add vendor/monocypher/src to the include-path too.
+
+#ifndef MONOCYPHER_H
+#include "../vendor/monocypher/src/monocypher.h"
+#endif
